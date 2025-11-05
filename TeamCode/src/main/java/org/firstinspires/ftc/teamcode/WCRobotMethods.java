87844cb4342194df0b4607954903c100af88f36f
WilliamChou123 <wchou28@shschools.org>
2025-11-04 17:57:22 -0800

    rename

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WCRobotMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WCRobotMethods.java
@@ -1,20 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
-
-import com.qualcomm.hardware.bosch.BHI260IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.robot.Robot;
 
 @TeleOp(name = "WCRobotNew", group = "Linear Opmode")
-public class WCRobotNewNew extends LinearOpMode {
+public class WCRobotMethods extends LinearOpMode {
     WCRobotNew robotNew;
 
     @Override
